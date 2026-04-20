fd1cf453bf49fc1c89b32187cf2b201a90fdca7e
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2026-04-20 15:07:33 -0300

    rmv header condition admin

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,8 @@
 "use client"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
 
 const Header = () => {
-    const pathname = usePathname()
-    if (pathname.includes('admin')) {
-        return null
-    }
     return (
         <header className="w-full flex justify-center border-b border-b-foreground/10 h-16">
             <div className="w-full container mx-auto flex justify-between items-center p-3 px-5 lg:px-0 text-sm">
